39fa23a314114efc630d926bea750c988a5ebe72
LERA <111573064+Lera1811@users.noreply.github.com>
2023-02-04 16:49:15 +0300

    Add files via upload

---
+++ Homework5/impls/view.java
@@ -0,0 +1,10 @@
+package homeWork5.Service.impls;
+
+ import homeWork5.Model.MathResult;
+
+ public interface View {
+     void showMathResult(MathResult result);
+     Integer enteringNumbers();
+     int showMenu();
+ }
+}
